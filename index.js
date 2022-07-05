c0a9bc23704958bb4558e2971663946a6952b661
Nicolas Mancilla Vergara <nico@nicomv.com>
2022-07-05 11:47:03 -0400

    Changed logging

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 
 const delay = process.env.DELAY ?? 6000;
+const label = 'Time taken';
 
 app.use((req, res, next) => {
     console.log('Delaying response by:', delay);
+    console.time(label);
     const id = setTimeout(() => {
-        console.log('Clearing timeout');
         clearTimeout(id);
-        console.log('Calling next');
         next();
     }, delay);
 });
 
 app.use((req, res) => {
+    console.timeEnd(label);
     return res.status(200).send("OK");
 })
 
